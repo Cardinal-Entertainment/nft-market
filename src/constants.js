d7bac3c942b00f02a7615c4146c6858af4df5dcf
Ryan <ryanpricelondon@gmail.com>
2021-10-22 19:31:45 -0400

    new artifact and address for market

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const marketContractAddress =
-  "0x4155b3752fb7AD9237Cc6c82532E4fdE84292EFC";
+  "0x22d4647013345cB0311c1D566558Df9f01c14b1b";
 export const zoomContractAddress = "0x8e21404bAd3A1d2327cc6D2B2118f47911a1f316";
 export const zoombiesContractAddress =
   "0x3E7997B8D30AA6216102fb2e9206246e478d57d3";
