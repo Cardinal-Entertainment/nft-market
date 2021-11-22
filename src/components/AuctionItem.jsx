b9b922e416d1920453a6a5cac3cb6f1cc899814b
prandev01 <76412910+prandev01@users.noreply.github.com>
2021-11-22 07:18:02 -0800

    remove console log

--- src/components/AuctionItem.jsx
+++ src/components/AuctionItem.jsx
@@ -290,8 +290,7 @@ const AuctionItem = ({
       default:
         throw new Error(`Unhandled currency type: ${currency}`);
     }
-
-    console.log("BID AMOUNT = ", amount)
+    
     const weiAmount = ethers.utils.parseEther(amount.toString());
 
     const approveTx = await currencyContract.approve(
