01accafaeb4c299ee6a7b666adcd2d3429ee729c
michael <michael.chi1997@gmail.com>
2021-12-04 12:06:21 -0500

    clean up view listing and merge code

--- src/hooks/useListing.js
+++ src/hooks/useListing.js
@@ -1,15 +1,15 @@
-import { useInfiniteQuery } from 'react-query';
-import axios from 'axios';
-import { apiEndpoint, QUERY_KEYS } from '../constants';
-import { getTokenSymbol } from '../utils/auction';
+import { useInfiniteQuery, useQuery } from 'react-query'
+import axios from 'axios'
+import { apiEndpoint, QUERY_KEYS } from '../constants'
+import { getTokenSymbol } from '../utils/auction'
 
 export const LISTING_PARAMS = {
   status: {
     ended: 'ENDED',
   },
-};
+}
 
-export const getAuctionListings = async (filters, nextOffset) => {
+const getAuctionListings = async (filters, nextOffset) => {
   const params = new URLSearchParams({
     cardOrigin: filters.cardType || '',
     saleToken: filters.token || '',
@@ -19,14 +19,14 @@ export const getAuctionListings = async (filters, nextOffset) => {
     offset: nextOffset,
     limit: '5',
     status: filters.status || '',
-  });
+  })
 
   const listings = await axios.get(
     `${apiEndpoint}/listings?${params.toString()}`
-  );
+  )
   // const listings = await axios.get(`http://localhost:3001/listings?${params.toString()}`)
 
-  const ar2 = listings.data.listings;
+  const ar2 = listings.data.listings
   return {
     data: ar2.map((listing) => ({
       ...listing,
@@ -35,8 +35,42 @@ export const getAuctionListings = async (filters, nextOffset) => {
     })),
     totalCount: parseInt(listings.data.count),
     nextOffset: parseInt(listings.data.nextOffset),
-  };
-};
+  }
+}
+
+const getSingleAuction = async (itemNumber, marketContract) => {
+  if (!marketContract) {
+    return null
+  }
+  const [listingResponse, bidsResponse, itemFromChain] = await Promise.all([
+    await axios.get(`${apiEndpoint}/item/${itemNumber}`),
+    await axios.get(`${apiEndpoint}/bids/${itemNumber}`),
+    await marketContract.getListItem(itemNumber),
+  ])
+
+  if (listingResponse.status === 200 && bidsResponse.status === 200) {
+    const { saleToken, lister } = listingResponse.data
+
+    const currency = getTokenSymbol(saleToken)
+
+    const isItemSettled =
+      itemFromChain === undefined ||
+      itemFromChain.seller === '0x0000000000000000000000000000000000000000'
+        ? true
+        : false
+
+    return {
+      id: itemNumber,
+      seller: lister,
+      currency,
+      bids: bidsResponse.data,
+      ...listingResponse.data,
+      isItemSettled,
+    }
+  }
+
+  throw new Error(`Failed to get auction: ${itemNumber}`)
+}
 
 export const useFetchListingQuery = (filters) => {
   return useInfiniteQuery(
@@ -44,10 +78,23 @@ export const useFetchListingQuery = (filters) => {
     ({ pageParam }) => getAuctionListings(filters, pageParam),
     {
       getNextPageParam: (lastPage) => {
-        if (lastPage.nextOffset > 0) return lastPage.nextOffset;
-        return undefined;
+        if (lastPage.nextOffset > 0) return lastPage.nextOffset
+        return undefined
       },
       refetchOnWindowFocus: false,
     }
-  );
-};
+  )
+}
+
+export const useFetchSingleListingQuery = (itemNumber, marketContract) => {
+  return useQuery(
+    [
+      QUERY_KEYS.listing,
+      { itemNumber, marketContractAddress: marketContract?.address },
+    ],
+    () => getSingleAuction(itemNumber, marketContract),
+    {
+      refetchOnWindowFocus: false,
+    }
+  )
+}
