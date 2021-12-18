300e765a16e5cbe55b8f9c41c3e303670e155397
Ryan <ryanpricelondon@gmail.com>
2021-12-18 15:34:54 -0500

    Updated the index metadata

--- src/hooks/useBlockchain.js
+++ src/hooks/useBlockchain.js
@@ -17,7 +17,9 @@ import {
 import { getWalletWMOVRBalance, getWalletZoomBalance } from '../utils/wallet'
 import watchMarketEvents from 'utils/setupWatcher'
 
-const isLocal = process.env.NODE_ENV === 'development'
+
+//const isLocal = process.env.NODE_ENV === 'development'
+const isLocal = true;
 
 const ethChainParam = isLocal
   ? {
@@ -67,7 +69,7 @@ const useBlockchain = () => {
         mustBeMetaMask: true,
       })
 
-      const provider = new ethers.providers.Web3Provider(metamaskProvider)      
+      const provider = new ethers.providers.Web3Provider(metamaskProvider)
       const signer = provider.getSigner()
       const [address, balance] = await Promise.all([
         signer.getAddress(),
