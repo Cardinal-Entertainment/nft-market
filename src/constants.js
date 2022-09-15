28ef65340f9f61bd17fa79a30b7df6563be10dc0
Ryan Price <ryanpricelondon@gmail.com>
2022-09-15 13:11:35 -0400

    updated rpc for moonbase alpha

--- src/constants.js
+++ src/constants.js
@@ -26,7 +26,7 @@ export const NETWORKS = {
     name: NETWORK_NAMES.MOONBASE,
     chainId: 1287,
     websocketRPC: `wss://moonbeam-alpha.api.onfinality.io/ws?apikey=${process.env['REACT_APP_MOONBEAM_RPC_API_KEY']}`,
-    httpRPC: `https://moonbeam-alpha.api.onfinality.io/rpc?apikey=${process.env['REACT_APP_MOONBEAM_RPC_API_KEY']}`,
+    httpRPC: `https://rpc.api.moonbase.moonbeam.network`,
     marketContractAddress: '0xd00d7b22deD10Fde1c94Be9E6D55D7190CCADD7F',
     zoomContractAddress: '0x8e21404bAd3A1d2327cc6D2B2118f47911a1f316',
     zoombiesContractAddress: '0x3E7997B8D30AA6216102fb2e9206246e478d57d3',
